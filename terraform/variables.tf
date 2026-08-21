87e7177eeb09c7e9e7b77063799099be454894ec
Alfredo Ball <alfredo.ball@outlook.com>
2026-08-21 03:07:01 -0500

    starter

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -13,3 +13,17 @@ variable "azure_environment" {
 variable "azure_speculative_resource_group_name" {
   type = string
 }
+
+variable "tfc_azure_dynamic_credentials" {
+  type = object({
+    default = object({
+      client_id_file_path  = string
+      oidc_token_file_path = string
+    })
+
+    aliases = map(object({
+      client_id_file_path  = string
+      oidc_token_file_path = string
+    }))
+  })
+}
